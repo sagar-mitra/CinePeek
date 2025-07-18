664fa59c35e132826a2b101514bc695cb23f0e92
Sagar Mitra <mitrasagar10@gmail.com>
2025-07-18 13:44:46 +0530

    feat: Header search query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import { USER_LOGO} from "../utils/constants"
+import { USER_LOGO } from "../utils/constants";
+import { motion, AnimatePresence } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+
 const Header = () => {
   const [active, isActive] = useState("Home");
+  const [searchIcon, setSearchIcon] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
   const navItems = [
     { name: "Home", to: "/home", id: 1 },
@@ -9,9 +15,29 @@ const Header = () => {
     { name: "Movies", to: "/movies", id: 3 },
   ];
 
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+  const navigate = useNavigate();
+
   const toggleActive = (name) => {
     isActive(name);
   };
+
+  // Handle search button
+  const handleSearch = () => {
+    if (isDesktop) {
+      if (searchText) {
+        navigate(`/search?query=${encodeURIComponent(searchText)}`);
+        // setSearchIcon(true);
+        setSearchText("");
+      } else {
+        setSearchIcon(!searchIcon);
+      }
+    } else {
+      navigate(`/search?query=${encodeURIComponent(searchText)}`);
+    }
+  };
+
+  console.log(window.innerWidth);
 
   return (
     <div className=" h-16 flex items-center justify-between fixed top-0 w-screen pl-10 pr-10 text-[var(--text-primary)] backdrop-blur-xl">
@@ -46,8 +72,27 @@ const Header = () => {
       {/* Right Side  */}
       <div className="flex gap-10 ">
         {/* Search  */}
-        <div>
-          <i class="bx  bx-search text-2xl md:text-3xl "></i>
+        <div className="flex items-center">
+          <i className="bx  bx-search text-2xl mr-1" onClick={handleSearch}></i>
+
+          <AnimatePresence>
+            {searchIcon && (
+              <motion.input
+                initial={{ opacity: 0, width: 0 }}
+                animate={{ opacity: 1, width: "14rem" }}
+                exit={{ opacity: 0, width: 0 }}
+                transition={{
+                  duration: 0.9,
+                  ease: "easeInOut",
+                }}
+                className={`font-medium outline-none border-b max-lg:hidden`}
+                type="text"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                placeholder="Search"
+              />
+            )}
+          </AnimatePresence>
         </div>
 
         {/* User Profile */}
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+  const query = searchParams.get("query");
+  console.log(query);
   return (
     <div>SearchPage</div>
   )

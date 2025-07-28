c4b7474cffffa461f30d1ad6a15b48ef80eb35de
Sagar Mitra <mitrasagar10@gmail.com>
2025-07-28 18:57:08 +0530

    feat: fix some glitch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,44 @@ import HorizontalScrollCard from "../components/HorizontalScrollCard";
 import { useSelector } from "react-redux";
 import useFetch from "../hooks/useFetch";
 
-
 const Home = () => {
   const trendingData = useSelector((store) => store.cinepeek.bannerData);
 
-  const popularData = useFetch("movie" ,"popular");
-  const topRatedData = useFetch("movie" ,"top_rated");
-  const upcomingData = useFetch("movie" ,"upcoming");
-  const popularTVData = useFetch("tv", "popular")
+  const popularData = useFetch("movie", "popular");
+  const topRatedData = useFetch("movie", "top_rated");
+  const upcomingData = useFetch("movie", "upcoming");
+  const popularTVData = useFetch("tv", "popular");
   if (!trendingData && !popularData && !topRatedData) return;
   return (
     <div className="text-[var(--text-primary)] bg-[var(--main-color)] min-h-screen">
-      <Banner type="movie"/>
-      <HorizontalScrollCard heading="Trending Movies" data={trendingData} type="movie"/>
-      <HorizontalScrollCard heading="Top Rated Movies" data={topRatedData} type="movie"/>
-      <HorizontalScrollCard heading="Popular Movies" data={popularData} type="movie"/>
-      <HorizontalScrollCard heading="Upcoming Movies" data={upcomingData} type="movie"/>
-      <HorizontalScrollCard heading="Popular TV Series" data={popularTVData} type="tv"/>
+      <Banner type="movie" />
+      <div className="mt-10 h-full">
+        <HorizontalScrollCard
+          heading="Trending Movies"
+          data={trendingData}
+          type="movie"
+        />
+        <HorizontalScrollCard
+          heading="Top Rated Movies"
+          data={topRatedData}
+          type="movie"
+        />
+        <HorizontalScrollCard
+          heading="Popular Movies"
+          data={popularData}
+          type="movie"
+        />
+        <HorizontalScrollCard
+          heading="Upcoming Movies"
+          data={upcomingData}
+          type="movie"
+        />
+        <HorizontalScrollCard
+          heading="Popular TV Series"
+          data={popularTVData}
+          type="tv"
+        />
+      </div>
     </div>
   );
 };
